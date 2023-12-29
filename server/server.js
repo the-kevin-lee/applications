835ff00de0f01482337e7e4d7914a2ef15325308
kevin <kl.klee.k.lee@gmail.com>
2023-12-28 21:47:35 -0500

    Untracking node_modules

--- server/server.js
+++ server/server.js
@@ -1,21 +1,56 @@
 const express = require('express');
-const path = require('path');
+const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = 8000;
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-    res.redirect('/home');
+
+app.use(cors());
+
+// CORS handling
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000/'); // Replace with your React app's origin
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
+
+
+
+
+
+
+// API routing 
+app.get('/api/weather', async (req, res) => {
+    console.log("Received request for /api/weather with city:", req.query.city);
+    const city = req.query.city;
+    const APIkey = "vzkgb5IUigg9TrPsg6Tiu7j98yILxd30";
+
+    const options = {
+        method: 'GET',
+        url: `https://api.tomorrow.io/v4/weather/realtime?location=${encodeURIComponent(city)}&apikey=${APIkey}`,
+        headers: {
+            accept: 'application/json',
+
+        }
+    };
+
+    console.log("Requesting external API with URL:", options.url);
+    try {
+        const response = await axios.request(options);
+        console.log("Data received:", response.data);
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error object:", error);
+        console.error("Error fetching data:", error.response ? error.response.data : error.message);
+        res.status(500).json({ error: 'Error fetching weather data' });
+    }
 });
 
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
+console.log("NODE_ENV is set to:", process.env.NODE_ENV);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
